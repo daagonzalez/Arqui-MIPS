96c02f3920f5659f0fa5bf8bbb8e7f7294c0fd02
Daniel Gonzalez <daa.gonzalez@gmail.com>
2018-06-14 14:45:04 -0600

    Lectura de archivo txt.

--- Arqui-MIPS/Form1.Designer.cs
+++ Arqui-MIPS/Form1.Designer.cs
@@ -29,28 +29,118 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtQuantum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtbOpenedFile = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rdbRapida = new System.Windows.Forms.RadioButton();
+            this.rdbLenta = new System.Windows.Forms.RadioButton();
+            this.lvSelectedFiles = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
-            // button1
+            // btnBrowse
             // 
-            resources.ApplyResources(this.button1, "button1");
-            this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnBrowse, "btnBrowse");
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtQuantum
+            // 
+            resources.ApplyResources(this.txtQuantum, "txtQuantum");
+            this.txtQuantum.Name = "txtQuantum";
+            this.txtQuantum.TextChanged += new System.EventHandler(this.txtQuantum_TextChanged);
+            this.txtQuantum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtQuantum_KeyPress);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // rtbOpenedFile
+            // 
+            resources.ApplyResources(this.rtbOpenedFile, "rtbOpenedFile");
+            this.rtbOpenedFile.Name = "rtbOpenedFile";
+            // 
+            // btnStart
+            // 
+            resources.ApplyResources(this.btnStart, "btnStart");
+            this.btnStart.Name = "btnStart";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // rdbRapida
+            // 
+            resources.ApplyResources(this.rdbRapida, "rdbRapida");
+            this.rdbRapida.Checked = true;
+            this.rdbRapida.Name = "rdbRapida";
+            this.rdbRapida.TabStop = true;
+            this.rdbRapida.UseVisualStyleBackColor = true;
+            // 
+            // rdbLenta
+            // 
+            resources.ApplyResources(this.rdbLenta, "rdbLenta");
+            this.rdbLenta.Name = "rdbLenta";
+            this.rdbLenta.UseVisualStyleBackColor = true;
+            // 
+            // lvSelectedFiles
+            // 
+            resources.ApplyResources(this.lvSelectedFiles, "lvSelectedFiles");
+            this.lvSelectedFiles.Name = "lvSelectedFiles";
+            this.lvSelectedFiles.UseCompatibleStateImageBehavior = false;
+            this.lvSelectedFiles.View = System.Windows.Forms.View.List;
             // 
             // FrmPrincipal
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lvSelectedFiles);
+            this.Controls.Add(this.rdbLenta);
+            this.Controls.Add(this.rdbRapida);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.rtbOpenedFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtQuantum);
+            this.Controls.Add(this.btnBrowse);
             this.Name = "FrmPrincipal";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtQuantum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtbOpenedFile;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rdbRapida;
+        private System.Windows.Forms.RadioButton rdbLenta;
+        private System.Windows.Forms.ListView lvSelectedFiles;
     }
 }
 
--- Arqui-MIPS/Form1.cs
+++ Arqui-MIPS/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,51 @@ namespace Arqui_MIPS
         public FrmPrincipal()
         {
             InitializeComponent();
+            openFileDialog1.Title = "Seleccione el archivo con los hilillos";
+            openFileDialog1.Filter = "Archivos txt|*.txt";
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            var fileSelected = openFileDialog1.ShowDialog();
+            foreach (var fileName in openFileDialog1.FileNames)
+            {
+                lvSelectedFiles.Items.Add(fileName);
+            }
+            rtbOpenedFile.Text = "";
+            if (fileSelected == DialogResult.OK)
+            {
+                string[] filelines = File.ReadAllLines(openFileDialog1.FileName);
+                foreach (string line in filelines)
+                {
+                    rtbOpenedFile.Text += line;
+                    if (line != filelines.Last<string>())
+                    {
+                        rtbOpenedFile.Text += "\n";
+                    }
+                }
+            }
+        }
+        
+        private void txtQuantum_TextChanged(object sender, EventArgs e)
+        {
+            if (lvSelectedFiles.Items.Count > 0 && txtQuantum.Text != "")
+            {
+                btnStart.Enabled = true;
+            }
+            else
+            {
+                btnStart.Enabled = false;
+            }
+        }
+
+        private void txtQuantum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
