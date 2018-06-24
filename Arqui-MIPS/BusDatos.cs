efd4ee910afdfa12ed29467926070475cfa4a33c
Daniel Gonzalez <daa.gonzalez@gmail.com>
2018-06-24 00:12:00 -0600

    Se creó la clase para el bus de datos y se comenzó la clase para la caché de datos.

---
+++ Arqui-MIPS/BusDatos.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Arqui_MIPS.CacheDatos;
+
+namespace Arqui_MIPS
+{
+    class BusDatos
+    {
+        //Parámetros de la clase
+        Memoria memoriaPrincipal;
+        //Nucleo n0;
+        //Nucleo n1;
+
+        /*
+         * Constructor de la clase
+         */
+        public BusDatos(Memoria memoria)
+        {
+            memoriaPrincipal = memoria;
+        }
+
+        /*
+         * Escribir bloque de caché a memoria
+         */
+        public void BloqueAMem(BloqueCacheDatos bloqueCache, int dirInicial)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                memoriaPrincipal.SetPalabraDato(dirInicial, i, bloqueCache.GetPalabra(i));
+            }
+        }
+
+        /*
+         * Leer bloque de memoria
+         */
+        public BloqueCacheDatos BloqueDeMem(int dirInicial)
+        {
+            BloqueCacheDatos bloqueCache = new BloqueCacheDatos();
+            for (int i = 0; i < 4; i++)
+            {
+                bloqueCache.SetPalabra(i, memoriaPrincipal.GetPalabraDato(dirInicial, i));
+            }
+            bloqueCache.SetEtiqueta(memoriaPrincipal.GetEtiquetaDato(dirInicial));
+            bloqueCache.SetEstado(BloqueCacheDatos.Estado.C); //Creo que siempre se cargan compartidos
+            return bloqueCache;
+        }
+
+        /*
+         * Copiar Bloque de una caché a la otra
+         */
+        public void ModificarPalabraCache(int nucleoDestino, int iBloque, BloqueCacheDatos elBloque)
+        {
+            if (nucleoDestino == 0)
+            {
+                //n0.cacheDatos.SetBloque(iBloque,elBloque); //En la caché de datos, bloque iBloque, colocar elBloque
+            }
+            else
+            {
+                //n1.cacheDatos.SetBloque(iBloque,elBloque); //En la caché de datos, bloque iBloque, colocar elBloque              
+            }
+        }
+
+        /*
+         * Modificar estado de un bloque en la caché de un núcleo
+         */
+        public void CambiarEstadoBloqueCache(int nucleo, int iBloque, int nEstado)
+        {
+            if (nucleo == 0)
+            {
+                //n0.cacheDatos.SetEstadoBloque(iBloque,nEstado); //En la caché de datos, bloque iBloque, colocar el estado nEstado               
+            }
+            else
+            {
+                //n1.cacheDatos.SetEstadoBloque(iBloque,nEstado); //En la caché de datos, bloque iBloque, colocar el estado nEstado
+            }
+        }
+        
+        /*
+         * Retorna el estado de un bloque en la caché de datos de un núcleo
+         */
+        public int GetEstadoBloqueCache(int nucleo, int iBloque)
+        {
+            int estado = 0;
+            if (nucleo == 0)
+            {
+                //estado = n0.cacheDatos.GetEstadoBloque(iBloque); //En la caché de datos, retornar el estado del bloque iBloque
+            }
+            else
+            {
+                //estado = n1.cacheDatos.GetEstadoBloque(iBloque); //En la caché de datos, retornar el estado del bloque iBloque
+            }
+            return estado;
+        }
+
+        /*
+         * Retorna la etiqueta de un bloque en la caché de datos de un núcleo
+         */
+        public int GetEtiquetaBloqueCache(int nucleo, int iBloque)
+        {
+            int etiqueta = 0;
+            if (nucleo == 0)
+            {
+                //etiqueta = n0.cacheDatos.GetEtiquetaBloque(iBloque); //En la caché de datos, retornar la etiqueta del bloque iBloque
+            }
+            else
+            {
+                //etiqueta = n1.cacheDatos.GetEtiquetaBloque(iBloque); //En la caché de datos, retornar la etiqueta del bloque iBloque
+            }
+            return etiqueta;
+        }
+    }
+}
---
+++ Arqui-MIPS/CacheDatos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arqui_MIPS
+{
+    class CacheDatos
+    {
+        private const int CANTIDAD_BLOQUES = 8; //Corroborar
+        BloqueCacheDatos[] bloques;
+
+        /*
+         * Constructor de la clase
+         */
+        public CacheDatos()
+        {
+            bloques = new BloqueCacheDatos[CANTIDAD_BLOQUES];
+            for (int i = 0; i < CANTIDAD_BLOQUES; i++)
+            {
+                bloques[i] = new BloqueCacheDatos();
+            }
+        }
+        public class BloqueCacheDatos
+        {
+            int[] palabras;
+            Estado estado;
+            int etiqueta;
+
+            public enum Estado {I, C, M}
+
+            public BloqueCacheDatos()
+            {
+                palabras = new int[4];
+                estado = Estado.I;
+                etiqueta = -1;
+            }
+
+            public int GetPalabra(int iPalabra)
+            {
+                return palabras[iPalabra];
+            }
+
+            public Estado GetEstado()
+            {
+                return estado;
+            }
+
+            public int GetEtiqueta()
+            {
+                return etiqueta;
+            }
+
+            public void SetPalabra(int iPalabra, int nPalabra)
+            {
+                palabras[iPalabra] = nPalabra;
+            }
+
+            public void SetEstado(Estado nEstado)
+            {
+                estado = nEstado;
+            }
+
+            public void SetEtiqueta(int nEtiqueta)
+            {
+                etiqueta = nEtiqueta;
+            }
+        }
+    }
+}
